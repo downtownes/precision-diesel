7e59ea6afc3ae6b5e5db6dfb795927f05dac63fc
downtownes <townes.falcon@gmail.com>
2018-04-22 15:16:38 -0600

    added side info bar to homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -7,7 +7,15 @@ import NavBar from '../NavBar/NavBar';
 export default function Home() {
     return (
         <div className="Home">
-            
+            <div className="welcomeContainer">
+                <h1>WELCOME TO PRECISION DIESEL SERVICE</h1>
+                <p className="companyDescription">We are a family owned and operated diesel mechanic service based in Preston, Idaho. We have been operating for more than twenty-three years to bring you all of your semi, diesel truck, and tractor needs.</p>
+                <div className="contactInfo">
+                    <h4>Contact Info</h4>
+                    <p>Phone: 208*852*2756</p>
+                    <p>Email: precisiondiesel@gmail.com</p>
+                </div>
+            </div>
         </div>
     )
 }
