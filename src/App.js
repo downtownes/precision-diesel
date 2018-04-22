770c404d915d172930d5c20e2b6e275c9bf33e2c
downtownes <townes.falcon@gmail.com>
2018-04-22 14:25:42 -0600

    added background photo to home page

--- src/App.js
+++ src/App.js
@@ -6,15 +6,15 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <NavBar3 />
+        <NavBar3 />
         {routes}
-        <div>
-                <div className="footerContainer">
-                    <h4 className="footerInfo">Contact Information</h4>
-                    <p className="footerInfo">Phone: 208*555*1692</p>
-                    <p className="footerInfo">Email: precisiondiesel@gmail.com</p>
-                </div>
-            </div>
+        {/* <div>
+          <div className="footerContainer">
+            <h4 className="footerInfo">Contact Information</h4>
+            <p className="footerInfo">Phone: 208*555*1692</p>
+            <p className="footerInfo">Email: precisiondiesel@gmail.com</p>
+          </div>
+        </div> */}
       </div>
     );
   }
